cb1b6a28abe78b2612fdf4068d1f2f99f21b346d
baris <baris.ock.etudes@gmail.com>
2025-06-13 15:18:16 +0200

    ajout des bouton pour recommencer et menu mais ne font rien pour le moment

--- app/src/main/java/com/example/memory/GameOver.kt
+++ app/src/main/java/com/example/memory/GameOver.kt
@@ -6,10 +6,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.material3.Text
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.Column
+
 
 
 @Composable
@@ -17,14 +22,37 @@ fun GameOver() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black.copy(alpha = 0.7f)),
-        contentAlignment = Alignment.Center // centre le contenu
+            .background(Color.Black.copy(alpha = 0.75f)),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Game Over", // ✍️ ton message
-            color = Color.White, // 🤍 texte blanc
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Game Over",
+                color = Color.White,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Button(
+                onClick = {}, // ne fait rien pour l’instant
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF4169E1),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text(text = "Recommencer")
+            }
+
+            Button(
+                onClick = {}, // ne fait rien pour l’instant
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF4169E1),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text(text = "menu")
+            }
+        }
     }
 }
